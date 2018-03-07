939563243a72246e61ceef384ba695765dc74487
DharaniDD <37145137+DharaniDD@users.noreply.github.com>
2018-03-07 18:10:37 +0530

    Create bank.sol

---
+++ bank.sol
@@ -0,0 +1,67 @@
+# solidity
+pragma solidity ^0.4.0;
+import "./Ownable.sol";
+contract bank
+{
+    address  owner;
+
+    string public name;
+    string public symbol;
+    uint256 public totalSupply;
+    mapping(address=>uint256)balance;
+    mapping(address=>mapping(address=>uint256))allowed;
+    function bank()
+    {
+        owner=msg.sender;
+        name="dharani";
+        symbol="$";
+        totalSupply=1000;
+        owner=msg.sender;
+        balance[owner]=totalSupply;
+    }
+         function total_supply()public  returns(uint256)
+     {
+              return totalSupply;
+     } 
+    function balanceOf(address owner)public constant returns(uint256)
+    {
+        return balance[owner];
+    }
+    function deposit(uint256 amt)public returns(uint256)
+    {
+        balance[owner]+=amt;
+        return(balance[owner]);
+    }
+     function withdraw(uint256 amt)public returns(uint256)
+    {
+        balance[owner]-=amt;
+        return(balance[owner]);
+    }
+     function transfer(address to,uint256 amt)public returns(uint256,uint256)
+    {
+        balance[owner]-=amt;
+        balance[to]+=amt;
+        return(balance[owner],balance[to]);
+    }
+    
+    function approval(address from,uint token)public  returns(uint)
+          {
+                    allowed[from][owner]=token;
+          } 
+           function allowance(address from)public view  returns(uint remain)
+          {
+              return allowed[from][owner];
+          }
+     function transferfrom(address from,address to,uint token)public  returns(uint,uint)
+      {
+        if(balance[from] >=token && token > 0 && allowed[from][to] >= token)
+        {
+             
+          balance[from]=balance[owner]-token;
+           balance[to]=balance[to]+token;
+          allowed[from][owner]=allowed[from][owner]-token;
+         
+          return  (balance[from], balance[to]);
+        }
+       }
+}
